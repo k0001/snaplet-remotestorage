1a8d925e5ed4a70c2d231fc6ec738105d9278d04
Renzo Carbonara <gnuk0001@gmail.com>
2013-02-19 16:42:18 -0300

    Try a more expressive approach to types using GADTs

--- src/Network/RemoteStorage/Types.hs
+++ src/Network/RemoteStorage/Types.hs
@@ -1,16 +1,26 @@
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE BangPatterns #-}
+
 
 module Network.RemoteStorage.Types
   ( ItemName
   , unItemName
-  , itemName
+  , mkItemName
   , validItemNameChar
-  , ItemVersion(..)
+  , ItemVersion
   , itemVersionSeconds
+  , itemVersionMilliseconds
+  , Folder
+  , Document
   , Node(..)
+  , mkNFolder
+  , nodeVersion
+  , lookupNFolder
+  , lookupNDocument
   , NodePath
-  , nodeRep
   , Request(..)
   , NodeRequest
   ) where
@@ -18,64 +28,86 @@ module Network.RemoteStorage.Types
 import qualified Data.Text as T
 import qualified Data.Aeson as J
 import           Data.Monoid ((<>))
-import           Data.Time.Clock (UTCTime)
-import           Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import           Data.Time.Clock.POSIX (POSIXTime)
+import           Data.Hashable (Hashable)
+import qualified Data.HashMap as M
+
 
 --------------------------------------------------------------------------------
 
--- | An 'ItemName' is wrapper around 'Text' that can only contain the following
--- characters: @a-z@, @A-Z@, @0-9@, @%@, @-@, @_@.
+-- | An 'ItemName' is wrapper around 'Text' that can only contain only valid
+-- item names.
 --
--- Use the smart constructor 'itemName' to build an 'ItemName'.
+-- Use the smart constructor 'mkItemName' to build an 'ItemName'.
 newtype ItemName = ItemName { unItemName :: T.Text }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Hashable, Ord)
 
--- | Returns 'Just' an 'ItemName' if the given 'T.Text' would be an acceptable
--- 'ItemName', otherwise 'Nothing'.
-itemName :: T.Text -> Maybe ItemName
-itemName t | T.all validItemNameChar t = Just $ ItemName t
+-- | 'Just' an 'ItemName' if the given 'T.Text' would be a valid 'ItemName',
+-- otherwise 'Nothing'.
+mkItemName :: T.Text -> Maybe ItemName
+mkItemName t | T.all validItemNameChar t = Just $ ItemName t
            | otherwise                 = Nothing
 
 -- | Whether the given 'Char' is one of: @a-z@, @A-Z@, @0-9@, @%@, @-@, @_@
 validItemNameChar :: Char -> Bool
 validItemNameChar c =
-    (c >= 'a'  &&  c <= 'z') ||
-    (c >= 'A'  &&  c <= 'Z') ||
-    (c >= '0'  &&  c <= '9') ||
-     c == '%'  ||  c == '-'  ||  c == '_'
-
-
-newtype ItemVersion = ItemVersion { unItemVersion :: UTCTime }
-                    deriving (Eq)
-
-instance Show ItemVersion where
-  show = show . itemVersionSeconds
-
-itemVersionSeconds :: ItemVersion -> Integer
-itemVersionSeconds = floor . utcTimeToPOSIXSeconds . unItemVersion
+    (c >= 'a'  &&  c <= 'z') || (c >= 'A'  &&  c <= 'Z') ||
+    (c >= '0'  &&  c <= '9') ||  c == '%'  ||  c == '-'  ||  c == '_'
 
 --------------------------------------------------------------------------------
 
-data Node
-  = Folder !ItemName !ItemVersion [Node]
-  | Document !ItemName !ItemVersion
-  deriving (Eq, Show)
+type ItemVersion = POSIXTime
+
+itemVersionSeconds :: ItemVersion -> Integer
+itemVersionSeconds = truncate
+
+itemVersionMilliseconds :: ItemVersion -> Integer
+itemVersionMilliseconds = truncate . (*1000)
+
+--------------------------------------------------------------------------------
+
+data Folder
+data Document
+
+type NodeMap a b x = M.Map ItemName (Node a b x)
+
+data Node a b x where
+  NFolder   :: ItemVersion -> a -> NodeMap a b x -> Node a b Folder
+  NDocument :: ItemVersion -> b                  -> Node a b Document
+
+
+-- | Construct an 'Node Folder' with an optional default 'ItemVersion'. If no
+-- 'ItemVersion' is given, then it is calculated from the given children.
+mkNFolder :: Maybe ItemVersion -> a -> NodeMap a b x -> Node a b Folder
+mkNFolder (Just ver) a xs = NFolder ver a xs
+mkNFolder Nothing    a xs = NFolder ver a xs
+  where ver = maximum . fmap nodeVersion $ M.elems xs
+
+
+instance J.ToJSON (Node a b Folder) where
+  toJSON (NFolder _ _ xs) = J.object $ M.foldWithKey pair [] xs
+    where
+      pair (ItemName n) (NFolder v _ _) = (:) $ (n <> "/") J..= showVersion v
+      pair (ItemName n) (NDocument v _) = (:) $  n         J..= showVersion v
+      showVersion = show . itemVersionMilliseconds
+
+nodeVersion :: Node a b x -> ItemVersion
+nodeVersion (NFolder v _ _) = v
+nodeVersion (NDocument v _) = v
+
+--------------------------------------------------------------------------------
 
 type NodePath = [ItemName]
 
---------------------------------------------------------------------------------
+lookupNFolder :: Node a b x -> NodePath -> Maybe (Node a b Folder)
+lookupNFolder x@(NFolder _ _ _) []     = Just x
+lookupNFolder   (NFolder _ _ m) (k:ks) = M.lookup k m >>= flip lookupNFolder ks
+lookupNFolder   _                _     = Nothing
 
-data NodeRep
-  = FolderRep J.Value
-  | DocumentRep ItemVersion
-  deriving (Eq, Show)
-
-nodeRep :: Node -> NodeRep
-nodeRep (Document _ v)    = DocumentRep v
-nodeRep (Folder   _ _ xs) = FolderRep . J.object $ map itemPair xs
-  where
-    itemPair (Document (ItemName n) v)   = (n <> "/") J..= show v
-    itemPair (Folder   (ItemName n) v _) =  n         J..= show v
+lookupNDocument :: Node a b x -> NodePath -> Maybe (Node a b Document)
+lookupNDocument x@(NDocument _ _) []     = Just x
+lookupNDocument   (NFolder _ _ m) (k:ks) = M.lookup k m >>= flip lookupNDocument ks
+lookupNDocument   _               _      = Nothing
 
 --------------------------------------------------------------------------------
 
